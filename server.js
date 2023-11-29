dae23d006a61ca83d9481d7e1191ce56f11da0cc
alex-gillis <alex.s.gillis@gmail.com>
2023-11-29 01:01:45 -0400

    convert ejs

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8060;
 const express = require(`express`);
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('Hello World!');
-});
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+    console.log(`Loaded!`);
+  });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on ${PORT}`);
+    console.log(`App listening on ${PORT}`);
 })
