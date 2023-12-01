33de9a3dc6b8f8b503a9f783dce2a0b16062c696
alex-gillis <alex.s.gillis@gmail.com>
2023-12-01 18:07:03 -0400

    function testing

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const PORT = process.env.PORT || 8060;
 const express = require(`express`);
 const app = express();
-
+// stylesheet
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
-  });
+  const bananas = "bananas";
+  console.log(res.body);
+  const templateVars = {bananas};
+  res.render('pages/index', templateVars);
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening on ${PORT}`);
-    console.log(`http://localhost:8060`);
+  console.log(`App listening on ${PORT}`);
+  console.log(`http://localhost:8060`);
 })
