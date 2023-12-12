2ae62a92c6d271fe12541bdfb8b92fd1b0275eb6
alex-gillis <alex.s.gillis@gmail.com>
2023-12-12 18:08:56 -0400

    add stats | expanded json options

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@ const storyJSON = '../../moduleOne.json';
 let myPages; 
 let myChoices;
 let myStory;
+let myHealth;
+let myOxygen;
+let myProfit;
+
+let currentHealth = 3;
+let currentO2 = 100;
+let totalProfit = 0;
+
+let playersPath = [];
 
 // test functions
 /* 
@@ -97,7 +106,7 @@ function onError() {
     console.log("*** Error has occured during fetch");
 }
 
-function popChoices(pgNum) {
+function popGame(pgNum) {
     const myPage = myPages[pgNum]; 
     // console.log('---page print out---', myPage)
     myStory.innerHTML = "";
@@ -105,13 +114,53 @@ function popChoices(pgNum) {
     myChoices.innerHTML = "";
     for (const myChoice of myPage.choices) {
         // console.log('---choice loop---', myChoice)
-        myChoices.innerHTML += `<button class="choices" onclick="popChoices(${myChoice.nextPage})">${myChoice.option}</button><br>`
+        myChoices.innerHTML += `<button class="choices" onclick="popGame(${myChoice.nextPage})">${myChoice.option}</button><br>`
     }
+
+    // calculating profits
+    totalProfit = myPage.profit + totalProfit;
+    myProfit.innerHTML = totalProfit;
+
+    // setting vitals
+    currentHealth = myPage.vitals + currentHealth;
+
+    if (currentHealth == 3) {
+        myHealth.innerHTML = "Healthy";
+    } else if (currentHealth == 2) {
+        myHealth.innerHTML = "Injured";
+    } else if (currentHealth == 1) {
+        myHealth.innerHTML = "Wounded";
+    } else if (currentHealth == 0) {
+        myHealth.innerHTML = "Dead";
+        // gonna have to put in whatever pgnumber it is with the appropriate death
+        if (myPage.death == undefined) {
+            popGame(myPage.death);
+        } else {
+            // popGame(generic death);
+        }
+    }
+
+    // filling oxygen 
+    currentO2 = myPage.oxygen + currentO2;
+    myOxygen.innerHTML = currentO2;
+
+    if (currentO2 == 0) {
+        // death from lack of air
+        // popGame(oxygen death);
+    }
+
+    // keeping track of where the player has been
+    playersPath.push(pgNum);
+
 }
 
 function main() {
     myChoices = document.getElementById("choices");
     myStory = document.getElementById("story");
+
+    myHealth = document.getElementById("health");
+    myOxygen = document.getElementById("oxygen");
+    myProfit = document.getElementById("profit");
 
     getJSONData(storyJSON, collectPages, onError);
 }
