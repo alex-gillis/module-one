52413eac8d38cff792760647d84ab956506f4ff2
alex-gillis <alex.s.gillis@gmail.com>
2024-01-28 22:08:40 -0400

    Complete Hanger Path || Deactivate Hatch Path

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // the path to retrieve the JSON
 const storyJSON = './moduleOne.json';
 // tracks if player has stumbled across special deaths
-const specialDeaths = [27, 29, 33];
+const specialDeaths = [27, 29, 33, 40];
 // this gives an alternate page depending on the player's actions 
 const altPages = [35];
 // referring to HTML objects associated with the storyline
